19a551def9b6312185b77b1cb9bb5492111087f7
Quentin Scaerou <quentin.scaerou@gmail.com>
2023-09-05 10:53:22 +0200

    Added UserDAO to DAOFactory

--- src/main/java/org/enchere/eni/m/dal/DAOFactory.java
+++ src/main/java/org/enchere/eni/m/dal/DAOFactory.java
@@ -1,5 +1,11 @@
 package org.enchere.eni.m.dal;
 
+import org.enchere.eni.m.dal.jdbc.UserDAOJdbcImpl;
+
 public class DAOFactory {
 
+	public static UserDAO getUserDAO() {
+		return new UserDAOJdbcImpl();
+	}
+	
 }
