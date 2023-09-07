0daa45a8ee4e3398b56bf7bb0a3366eb6fe5fd7a
whasani2023 <whasani2023@129SE23-8D4MMS3.ad.campus-eni.fr>
2023-09-07 09:59:56 +0200

    Merge branch 'master' of https://github.com/Ev3ntyr/JAVA_Projects.git

--- src/main/java/org/enchere/eni/c/Home.java
+++ src/main/java/org/enchere/eni/c/Home.java
@@ -6,8 +6,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-import org.enchere.eni.m.bll.ItemSoldManager;
+import org.enchere.eni.m.bll.ItemManager;
+
+import org.enchere.eni.m.bo.Item;
 
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +18,7 @@ public class Home extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		List<ItemSold> listItemSold = ItemSoldManager.getInstance().selectAll();
+		List<Item> listItem = ItemManager.getInstance().selectAll();
 		
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
