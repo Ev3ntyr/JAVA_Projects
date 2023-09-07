a3024ae3f484a202ae6d8f2eb5d5b2ea6d9d33c6
whasani2023 <whasani2023@129SE23-8D4MMS3.ad.campus-eni.fr>
2023-09-07 09:57:14 +0200

    jspHome listItemSold getInstance

--- src/main/java/org/enchere/eni/c/Home.java
+++ src/main/java/org/enchere/eni/c/Home.java
@@ -7,11 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.enchere.eni.m.bll.ItemSoldManager;
+
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<ItemSold> listItemSold = ItemSoldManager.getInstance().selectAll();
+		
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
 		rd.forward(request, response);
