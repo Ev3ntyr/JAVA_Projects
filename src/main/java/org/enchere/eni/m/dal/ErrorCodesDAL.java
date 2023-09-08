1dcd79851e6ec5c8664389bc10c6fc605288a0da
Ev3ntyr <fanny.santamaria2023@campus-eni.fr>
2023-09-08 14:57:52 +0200

    Adding DAL error codes

--- src/main/java/org/enchere/eni/m/dal/ErrorCodesDAL.java
+++ src/main/java/org/enchere/eni/m/dal/ErrorCodesDAL.java
@@ -2,7 +2,7 @@ package org.enchere.eni.m.dal;
 
 public abstract class ErrorCodesDAL {
 
-	public final static int ALIAS_NOT_UNIQUE_ERROR = 40_001;	
+	public final static int INSERT_ITEM_DAL_ERROR = 40_001;	
 
 	
 }
